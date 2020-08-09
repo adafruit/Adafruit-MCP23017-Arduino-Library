47b97e9666a226519f2edb1aad5eec16d315d940
André Fiedler <mail@andrefiedler.de>
2020-08-10 01:06:43 +0200

    simplification

--- examples/custom_frequency/custom_frequency.ino
+++ examples/custom_frequency/custom_frequency.ino
@@ -4,15 +4,13 @@
 // Sample for usage of two MCP23017 I/O expander with an ESP32
 // public domain!
 
-TwoWire myWire = TwoWire(0);
-
 Adafruit_MCP23017 mcp;
   
 void setup() {  
 
-  myWire.setClock(1700000); // set frequency to 1.7mhz
+  Wire.setClock(1700000); // set frequency to 1.7mhz
 
-  mcp.begin(&myWire); // use default address 0
+  mcp.begin(&Wire); // use default address 0
 }
 
 void loop() {
--- examples/use_two_i2c/use_two_i2c.ino
+++ examples/use_two_i2c/use_two_i2c.ino
@@ -4,19 +4,16 @@
 // Sample for usage of two MCP23017 I/O expander with an ESP32
 // public domain!
 
-TwoWire firstWire = TwoWire(0);
-TwoWire secondWire = TwoWire(1);
-
 Adafruit_MCP23017 firstMcp;
 Adafruit_MCP23017 secondMcp;
   
 void setup() {  
 
-  firstWire.begin(21, 22);
-  secondWire.begin(19, 23);
+  Wire.begin(21, 22);
+  Wire1.begin(4, 5);
 
-  firstMcp.begin(&firstWire); // use default address 0 for I2C #1
-  secondMcp.begin(&secondWire); // use default address 0 for I2C #2
+  firstMcp.begin(&Wire); // use default address 0 for I2C #1
+  secondMcp.begin(&Wire1); // use default address 0 for I2C #2
 }
 
 void loop() {
