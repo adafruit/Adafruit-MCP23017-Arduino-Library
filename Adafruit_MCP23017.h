571ae312b5783402b48f8a487605842eaf4661c4
Richard Teel <richteel@teelsys.com>
2021-04-11 17:16:33 -0400

    Reverting to original check-in to see clang output as I now believe I understand how to fix

--- Adafruit_MCP23017.h
+++ Adafruit_MCP23017.h
@@ -1,6 +1,6 @@
 /*!
-   @file Adafruit_MCP23017.h
-*/
+ * @file Adafruit_MCP23017.h
+ */
 
 #ifndef _Adafruit_MCP23017_H_
 #define _Adafruit_MCP23017_H_
@@ -19,8 +19,8 @@
 #endif
 
 /*!
-   @brief MCP23017 main class
-*/
+ * @brief MCP23017 main class
+ */
 class Adafruit_MCP23017 {
 public:
   void begin(uint8_t addr, TwoWire *theWire = &Wire);
@@ -46,7 +46,7 @@ public:
 
 private:
   uint8_t i2caddr;
-  TwoWire *_wire; // pointer to a TwoWire object
+  TwoWire *_wire; //!< pointer to a TwoWire object
 
   uint8_t bitForPin(uint8_t pin);
   uint8_t regForPin(uint8_t pin, uint8_t portAaddr, uint8_t portBaddr);
@@ -55,45 +55,45 @@ private:
   void writeRegister(uint8_t addr, uint8_t value);
 
   /**
-     Utility private method to update a register associated with a pin (whether
-     port A/B) reads its value, updates the particular bit, and writes its
-     value.
-  */
+   * Utility private method to update a register associated with a pin (whether
+   * port A/B) reads its value, updates the particular bit, and writes its
+   * value.
+   */
   void updateRegisterBit(uint8_t p, uint8_t pValue, uint8_t portAaddr,
                          uint8_t portBaddr);
 };
 
-#define MCP23017_ADDRESS 0x20 // MCP23017 Address
+#define MCP23017_ADDRESS 0x20 //!< MCP23017 Address
 
 // ports
 #define MCP23017_PORT_A 0x00
 #define MCP23017_PORT_B 0x01
- 
+
 // registers
-#define MCP23017_IODIRA 0x00 // I/O direction register A
-#define MCP23017_IPOLA 0x02 // Input polarity port register A
-#define MCP23017_GPINTENA 0x04 // Interrupt-on-change pins A
-#define MCP23017_DEFVALA 0x06 // Default value register A
-#define MCP23017_INTCONA 0x08 // Interrupt-on-change control register A
-#define MCP23017_IOCONA 0x0A // I/O expander configuration register A
-#define MCP23017_GPPUA 0x0C // GPIO pull-up resistor register A
-#define MCP23017_INTFA 0x0E // Interrupt flag register A
-#define MCP23017_INTCAPA 0x10 // Interrupt captured value for port register A
-#define MCP23017_GPIOA 0x12 // General purpose I/O port register A
-#define MCP23017_OLATA 0x14 // Output latch register 0 A
+#define MCP23017_IODIRA 0x00   //!< I/O direction register A
+#define MCP23017_IPOLA 0x02    //!< Input polarity port register A
+#define MCP23017_GPINTENA 0x04 //!< Interrupt-on-change pins A
+#define MCP23017_DEFVALA 0x06  //!< Default value register A
+#define MCP23017_INTCONA 0x08  //!< Interrupt-on-change control register A
+#define MCP23017_IOCONA 0x0A   //!< I/O expander configuration register A
+#define MCP23017_GPPUA 0x0C    //!< GPIO pull-up resistor register A
+#define MCP23017_INTFA 0x0E    //!< Interrupt flag register A
+#define MCP23017_INTCAPA 0x10  //!< Interrupt captured value for port register A
+#define MCP23017_GPIOA 0x12    //!< General purpose I/O port register A
+#define MCP23017_OLATA 0x14    //!< Output latch register 0 A
 
-#define MCP23017_IODIRB 0x01 // I/O direction register B
-#define MCP23017_IPOLB 0x03 // Input polarity port register B
-#define MCP23017_GPINTENB 0x05 // Interrupt-on-change pins B
-#define MCP23017_DEFVALB 0x07 // Default value register B
-#define MCP23017_INTCONB 0x09 // Interrupt-on-change control register B
-#define MCP23017_IOCONB 0x0B // I/O expander configuration register B
-#define MCP23017_GPPUB 0x0D // GPIO pull-up resistor register B
-#define MCP23017_INTFB 0x0F // Interrupt flag register B
-#define MCP23017_INTCAPB 0x11 // Interrupt captured value for port register B
-#define MCP23017_GPIOB 0x13 // General purpose I/O port register B
-#define MCP23017_OLATB 0x15 // Output latch register 0 B
+#define MCP23017_IODIRB 0x01   //!< I/O direction register B
+#define MCP23017_IPOLB 0x03    //!< Input polarity port register B
+#define MCP23017_GPINTENB 0x05 //!< Interrupt-on-change pins B
+#define MCP23017_DEFVALB 0x07  //!< Default value register B
+#define MCP23017_INTCONB 0x09  //!< Interrupt-on-change control register B
+#define MCP23017_IOCONB 0x0B   //!< I/O expander configuration register B
+#define MCP23017_GPPUB 0x0D    //!< GPIO pull-up resistor register B
+#define MCP23017_INTFB 0x0F    //!< Interrupt flag register B
+#define MCP23017_INTCAPB 0x11  //!< Interrupt captured value for port register B
+#define MCP23017_GPIOB 0x13    //!< General purpose I/O port register B
+#define MCP23017_OLATB 0x15    //!< Output latch register 0 B
 
-#define MCP23017_INT_ERR 255 // Interrupt error
+#define MCP23017_INT_ERR 255 //!< Interrupt error
 
 #endif
