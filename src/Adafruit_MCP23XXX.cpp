540acfcf99819d2a1a4381db4f05b4aead641fd7
Jesus Vaquerizo <32816191+jvaque@users.noreply.github.com>
2022-12-07 23:51:37 +0000

    Refactor getLastInterruptPin() by returning early
    Changed function logic more in line to how it was first implemented in
    v1.3.0 by @PsuFan

--- src/Adafruit_MCP23XXX.cpp
+++ src/Adafruit_MCP23XXX.cpp
@@ -225,7 +225,6 @@ void Adafruit_MCP23XXX::clearInterrupts() {
 */
 /**************************************************************************/
 uint8_t Adafruit_MCP23XXX::getLastInterruptPin() {
-  uint8_t intpin = MCP23XXX_INT_ERR;
   uint8_t intf;
 
   // Port A
@@ -234,25 +233,23 @@ uint8_t Adafruit_MCP23XXX::getLastInterruptPin() {
   INTFA.read(&intf);
   for (uint8_t pin = 0; pin < 8; pin++) {
     if (intf & (1 << pin)) {
-      intpin = pin;
-      break;
+      return pin;
     }
   }
 
-  // Port B and still not found?
-  if ((pinCount > 8) && (intpin == MCP23XXX_INT_ERR)) {
+  // Port B ?
+  if (pinCount > 8) {
     Adafruit_BusIO_Register INTFB(i2c_dev, spi_dev, MCP23XXX_SPIREG,
                                   getRegister(MCP23XXX_INTF, 1));
     INTFB.read(&intf);
     for (uint8_t pin = 0; pin < 8; pin++) {
       if (intf & (1 << pin)) {
-        intpin = pin + 8;
-        break;
+        return pin + 8;
       }
     }
   }
 
-  return intpin;
+  return MCP23XXX_INT_ERR;
 }
 
 /**************************************************************************/
