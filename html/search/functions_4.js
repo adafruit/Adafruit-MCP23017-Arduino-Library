f03be76044657180692a7790284817e79db863aa
Doxygen CI <ci-arduino@invalid>
2022-12-13 20:28:45 +0000

    Deploy docs to GitHub Pages from commit 2119e350e5
    Commit: 2119e350e5ee0ceb4dec0b15105345e75ccb64da
    GitHub Actions run: 3680214538

--- html/search/functions_4.js
+++ html/search/functions_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['enableaddrpins',['enableAddrPins',['../class_adafruit___m_c_p23_x17.html#a1ca84f6e1a907c973f030332e56affc1',1,'Adafruit_MCP23X17']]]
+  ['enableaddrpins',['enableAddrPins',['../class_adafruit___m_c_p23_x08.html#a8c41b799f09bf70be9549115c9742b7a',1,'Adafruit_MCP23X08::enableAddrPins()'],['../class_adafruit___m_c_p23_x17.html#a1ca84f6e1a907c973f030332e56affc1',1,'Adafruit_MCP23X17::enableAddrPins()']]]
 ];
